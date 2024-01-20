b0bf012b8c5da0e8b1b55e92e1be184eba3c7314
Nazli Zamanian <nazlizamanian@gmail.com>
2024-01-20 16:29:25 +0100

    commit

--- index.js
+++ index.js
@@ -4,6 +4,17 @@ const port = 3000;
 
 const stores= require('./stores.json');
 
+//-----------------------GET-------------------------------------
+app.get('/', function (req, res) {
+    const { storename } = req.query
+    console.log(storename)
+    const index = stores.findIndex(store => store.name === storename)
+    if (index > -1) {
+      res.json(stores[index])
+    } else {
+      res.send('Store not found!')
+    }
+  });
 
 
 //-----------------------DELETE-----------------------------------
