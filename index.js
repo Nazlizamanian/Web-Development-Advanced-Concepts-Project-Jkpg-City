ae4b3da594782c1e48956297fe4cc332e36f4f2e
mariagaric <maria.garic@hotmail.com>
2024-01-21 14:05:33 +0100

    commit

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const port = 3000;
 const stores= require('./stores.json');
 
 //////////////////////////////////////////////
-/////////////////////////////////bbbbbbbbbbbbbbbbbbbbbbbb
 //-----------------------GET-------------------------------------
 app.get('/', function (req, res) {
     const { storename } = req.query
