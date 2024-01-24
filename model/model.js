7634a0cb4c0cd98e3ce906285770899888fd1363
Nazli Zamanian <nazlizamanian@gmail.com>
2024-01-24 17:21:13 +0100

    databases and other stuff

--- model/model.js
+++ model/model.js
@@ -1,10 +1,65 @@
-const pg = require('pg');
-const { Database } = require('sqlite3');
+const { Client } = require('pg');
+const storeJson = require('../stores.json');
 
-class DB{
-    constructor(){
-        this.DB = new pg.Pool({
-            Database:'database'
-        });
+class Model {
+  constructor() {
+    this.client = new Client({
+      user: 'postgres',
+      host: 'localhost',
+      database: 'postgres',
+      password: '12345',
+      port: 5432,
+    });
+  }
+
+  //connects to the database
+  async initDatabase() {
+    await this.client.connect();
+  }
+
+  async setup(storeJson) {
+    await this.client.query(`
+      CREATE TABLE IF NOT EXISTS public.stores
+      (
+          id SERIAL NOT NULL,
+          name text,
+          url text,
+          district text,
+          CONSTRAINT stores_pkey PRIMARY KEY (id)
+      );
+    `);
+
+    await this.client.query(`
+      ALTER TABLE IF EXISTS public.stores OWNER to postgres;
+    `);
+
+    for (const store of storeJson) {
+      const checkForStore = await this.client.query(`
+        SELECT * FROM public.stores
+        WHERE name = $1
+        LIMIT 1`, [store.name]);
+
+      console.log(checkForStore.rows);
+
+      if (checkForStore.rows.length === 0) {
+        await this.client.query(`
+          INSERT INTO public.stores (name, url, district)
+          VALUES ($1, $2, $3)
+        `, [store.name, store.url, store.district]);
+      }
     }
+
+
+  }
+
+  async getAllStores() {
+    const query = 'SELECT * FROM stores';
+    const { rows }= await this.client.query(query);
+    
+    return rows;
+  }
+
 }
+
+module.exports = Model;
+
--- view/view.js
+++ view/view.js
@@ -1,6 +1,39 @@
 
-app.get('/stores',(req, res)=>{
-    console.log('stores')
-    db.all("SELECT * FROM stores", function(error, ))
 
+
+//-----------------------Stores?district=Öster-----------------------
+app.get('/districts', (req, res)=>{
+
+    const district = {};
+    
+        for (const store of stores) {
+            if(store.district==='Öster'){
+                district[store.district] = true; 
+            }    
+        }
+        res.json(Object.keys(district));
+  
 });
+
+//-----------------------DELETE-----------------------------------
+app.delete('/stores', (req, res)=>{
+    const {search } = req.query;
+    const storeIndex = stores.findIndex((row)=>{
+
+    });
+
+    if(storeIndex== -1){
+        res.send(`Store not found`);
+    }
+    else{
+        stores.splice(storeIndex, 1);
+    }
+});
+
+
+//-----------------------POST-----------------------------------
+app.post('/stores', 
+express.json(),
+(req,res)=> {
+    const {body } = req; 
+}); 
